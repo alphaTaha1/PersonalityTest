319a15a4312aadd8e18a23ad0c6ded26967c8b23
alphaTaha1 <tahanaveedfz@gmail.com>
2024-10-16 14:45:24 +0500

    empty

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
-import { BrowserRouter,Routes,Route} from "react-router-dom";
 import Header from "./components/Header.jsx"
-import Ooga from "./components/Ooga.jsx";
+
 import { useState } from "react";
 
 import Username from "./components/Username.jsx"
