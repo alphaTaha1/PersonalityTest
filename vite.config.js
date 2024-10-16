0ac98a23dced48096dd80c1b4f322235607ad94f
alphaTaha1 <tahanaveedfz@gmail.com>
2024-10-16 15:29:12 +0500

    yes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    watch:
-    {
+  server: {
+    watch: {
       usePolling: true,
-    }
+    },
   },
   plugins: [react()],
- 
-})
+  base: '/PersonalityTest/', // This should match your GitHub repository name
+});
