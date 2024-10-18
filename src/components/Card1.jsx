d9dc805459bccb659eac162dfd306dc3f1ede5cc
alphaTaha1 <tahanaveedfz@gmail.com>
2024-10-18 16:39:44 +0500

    DDD

--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import Ooga from "./Ooga";
+
 function Card1()
 {
+  const openOoga = () =>
+    {
+  
+        setOpenOoga(true)
 
+   handleVisibility()
+   }
 const [cardActive, isCardActive] = useState(true)
 const [fadeOut, isFadeOut] = useState(false)
-
-   const handleVisibility = () =>
+const [ooga, setOpenOoga] = useState(false)
+    const handleVisibility = () =>
     {
       isFadeOut(true)
       setTimeout(
@@ -14,27 +21,40 @@ const [fadeOut, isFadeOut] = useState(false)
         {
           isCardActive(false)
         }
-        ,1500
+        ,200
       )
      
     }
+  
+    // onClick={handleVisibility} 
+    //       className={`card ${fadeOut ? 'Fade-out' : ''}`} 
 
        if(cardActive)
        {
         return(
-          <div onClick={handleVisibility} 
-          className={`card ${fadeOut ? 'Fade-out' : ''}`} >
-          <div id="line"></div>
-          <p>Press this button to start</p>
+          <div className="card">
+            <h1>Select any of the following to continue playing</h1>
+            <button onClick={openOoga} className="button1">Personality Tests</button>
+            <button className="button2">IQ Test</button>
+            <button className="button3">More coming soon</button>
+
+
         </div>  
         );
+       
+
        }
-       if(fadeOut)
-        {
-         return(
-          <Ooga />
-         )
-        }
+       console.log("HI")
+       
+       if(ooga)
+       { 
+        console.log("worked")
+        return(
+        <Ooga />
+      )
+        
+       }
+    
    return null
 }
 export default Card1;
