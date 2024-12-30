992e33f50b09cbbd6486a727db9376d461788a9d
alphaTaha1 <tahanaveedfz@gmail.com>
2024-12-30 22:10:25 +0500

    Updates

--- src/description.jsx
+++
@@ -1,8 +0,0 @@
-function Description()
-{
-return(
-
-    <h1 id="discription">This game is designed for you to understand your personality (i dont know what i am doing)</h1>
-)
-}
-export default Description
